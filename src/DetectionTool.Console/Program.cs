7cf00d17d98543f8789f4a3bdef206947314f383
哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>
2023-06-09 20:38:19 +0900

    Add files via upload
    Signed-off-by: 哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>

--- src/DetectionTool.Console/Program.cs
+++ src/DetectionTool.Console/Program.cs
@@ -10,26 +10,26 @@ static void RunScan() {
 
     if (results.Detected) {
       Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine("Detected: YES");
-      Console.WriteLine("Possible Malware traces were detected on your machine");
+      Console.WriteLine("結果:検出されました");
+      Console.WriteLine("マルウェアの痕跡が検出された可能性があります");
       Console.WriteLine();
-      Console.WriteLine("Suspicious Files:");
+      Console.WriteLine("不審なファイル:");
       foreach(var detectedFile in results.DetectedFiles) {
         Console.WriteLine($"  => {detectedFile}");
       }
       Console.WriteLine();
       Console.ForegroundColor = ConsoleColor.Blue;
-      Console.WriteLine($"For instructions on what to do next go to: {DetectionTool.Core.Constants.kSupportArticle}");
+      Console.WriteLine($"次に行うべき事については、こちらをご覧下さい: {DetectionTool.Core.Constants.kSupportArticle}");
 
     } else {
       Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Detected: NO");
-      Console.WriteLine("Malware was not detected on your machine");
+      Console.WriteLine("結果:未検出");
+      Console.WriteLine("マルウェアは検出されませんでした");
     }
   } catch (Exception ex) {
     Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("Detected: Inconclusive");
-    Console.WriteLine($"Scan failed. {ex.Message}");
+    Console.WriteLine("結果:不確定");
+    Console.WriteLine($"スキャンに失敗しました {ex.Message}");
   }
 
   Console.ResetColor();
