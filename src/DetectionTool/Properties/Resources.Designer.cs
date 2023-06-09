2af4f7e0ce0f5abd16f6a8326bb48e20eceb2f16
哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>
2023-06-09 21:22:16 +0900

    Add files via upload
    Signed-off-by: 哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>

--- src/DetectionTool/Properties/Resources.Designer.cs
+++ src/DetectionTool/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DetectionToolJP.Properties {
+namespace DetectionTool.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DetectionToolJP.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DetectionToolJP.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DetectionTool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
